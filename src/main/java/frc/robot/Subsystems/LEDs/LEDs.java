baf747cd55750041f9c503e3c0ff5d5458a1edb1
Leona Wu <leonawu531@outlook.com>
2025-02-11 17:12:56 -0800

    detected April tag LEDs

--- src/main/java/frc/robot/Subsystems/LEDs/LEDs.java
+++ src/main/java/frc/robot/Subsystems/LEDs/LEDs.java
@@ -7,8 +7,11 @@ import edu.wpi.first.wpilibj.util.Color;
 import static edu.wpi.first.units.Units.Seconds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Subsystems.Vision.Vision;
+
 public class LEDs extends SubsystemBase {
     private static LEDs instance;
+    private Vision vision;
 
     public static LEDs getInstance() {
         if (instance == null) {
@@ -21,7 +24,7 @@ public class LEDs extends SubsystemBase {
     
 
     public LEDs() {
-         leds = new AddressableLED(0);
+         leds = new AddressableLED(0); //PWM
          ledBuffer = new AddressableLEDBuffer(1000); //long strip length is 1000
     }
 
@@ -30,6 +33,7 @@ public class LEDs extends SubsystemBase {
     }
 
     public enum ledStates{
+        OFF(0, 0, 0),
         ORANGE(5, 255, 255),
         BLUE(120, 255, 255),
         RED(0, 255, 255);
@@ -62,5 +66,15 @@ public class LEDs extends SubsystemBase {
 
         leds.setData(ledBuffer);
         leds.start();
+    }
+
+    public void detectAprilTag(){
+        //april tag detected
+        if(vision.getLatestAprilTagResult().hasTargets()){
+            setColor(ledStates.RED);
+        } else {
+            setColor(ledStates.OFF);
+        }
+        
     }
 }
