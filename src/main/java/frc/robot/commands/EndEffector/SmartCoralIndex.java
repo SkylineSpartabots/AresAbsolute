7023b8ff82aa354e91295696a132c8ef27990085
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2025-02-24 13:33:04 -0800

    Outtake with lasercan

--- src/main/java/frc/robot/commands/EndEffector/SmartCoralIndex.java
+++ src/main/java/frc/robot/commands/EndEffector/SmartCoralIndex.java
@@ -5,23 +5,17 @@ import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Subsystems.EndEffector;
 import frc.robot.Subsystems.EndEffector.OuttakeState;
 
-public class SetOuttake extends Command {
+public class SmartCoralIndex extends Command {
   EndEffector s_EndEffector;
-  Timer timer = new Timer();
-  boolean finished = false;
-  OuttakeState state;
 
-  public SetOuttake(OuttakeState state) {
+  public SmartCoralIndex() {
     s_EndEffector = EndEffector.getInstance();
     addRequirements(s_EndEffector);
-    this.state = state;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    timer.restart();
-    s_EndEffector.setOuttakeSpeed(state);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -32,13 +26,12 @@ public class SetOuttake extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    s_EndEffector.setOuttakeSpeed(0);
+    new SetOuttake(OuttakeState.INDEX);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (state == OuttakeState.INDEX && timer.hasElapsed(0.09)
-     || (state == OuttakeState.SCOREMID && timer.hasElapsed(0.5)));
+    return s_EndEffector.getLaserMeasurement().distance_mm < 3;
   }
 }
